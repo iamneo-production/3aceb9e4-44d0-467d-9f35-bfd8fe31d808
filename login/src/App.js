34e34793575d44c8ad64d7f975887b2b27cf4a3c
REETHIKAKAKANI <reethikakakani@gmail.com>
2022-09-28 12:08:12 +0000

    commit

--- login/src/App.js
+++ login/src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import Pop-up from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 
-export default function PopupGfg(){
-  return()
-}
 function App() {
   return (
     <div className="
